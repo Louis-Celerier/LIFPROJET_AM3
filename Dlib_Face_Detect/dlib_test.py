45165c3bffb96466ca35590b8ba7d9e0986f6012
Corentin <corentin.lussiez@gmail.com>
2021-02-02 16:00:43 +0100

    requirements.txt added

--- Dlib_Face_Detect/dlib_test.py
+++ Dlib_Face_Detect/dlib_test.py
@@ -1,6 +1,10 @@
-import numpy as np
 import cv2
 import dlib
 import sys
+import tensorflow as tf
+
+print(tf.test.is_gpu_available(
+    cuda_only=False, min_cuda_compute_capability=None)
+    )
 
 print(sys.version)
